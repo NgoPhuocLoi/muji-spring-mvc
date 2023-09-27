0f3e45cd65859d39f2d24a971418b63103a62387
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-09-27 21:24:17 +0700

    Config tailwindCSS

--- src/main/java/com/example/muji/config/WebSecurityConfig.java
+++ src/main/java/com/example/muji/config/WebSecurityConfig.java
@@ -1,5 +1,16 @@
 package com.example.muji.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class WebSecurityConfig {
 
+    @Bean
+    SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
+        return http.build();
+    }
 }
