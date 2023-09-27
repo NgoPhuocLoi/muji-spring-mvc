90367b37cf7e379ad55639e16970fbeeea372ec8
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-09-27 22:39:38 +0700

    Building Header component

--- src/main/java/com/example/muji/controllers/HomeController.java
+++ src/main/java/com/example/muji/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.muji.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String displayHome() {
+    public String displayHome(Model model) {
+        List<String> menuItems = Arrays.asList("Firniture", "Homeware", "Women", "Men", "Kids", "Stationery", "Beauty",
+                "Travel", "Food", "Sale", "Sustainability");
+        model.addAttribute("menuItems", menuItems);
         return "home.html";
     }
 }
